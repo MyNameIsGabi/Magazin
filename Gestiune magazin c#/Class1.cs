3791085b69c28b6b309870d89ad433b4bb828da3
MyNameIsGabi <128059085+MyNameIsGabi@users.noreply.github.com>
2023-03-16 16:25:05 +0200

    Add files via upload

---
+++ Gestiune magazin c#/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GestiuneMagazin
+{
+    class Aliment
+    {
+        public string Nume { get; set; }
+        public int Numar { get; set; }
+        public float Pret { get; set; }
+    }
+
+    class Magazin
+    {
+        private Aliment[] alimente = new Aliment[100];
+        private int numAlimente = 0;
+
+        public void AdaugaAliment()
+        {
+
+        }
+
+        public void ModificaAliment()
+        {
+
+        }
+
+        public void StergeAliment()
+        {
+            
+        }
+
+        public void AfiseazaAliment()
+        {
+
+        }
+
+        public void CautaAliment()
+        {
+
+        }
+    }
+}
---
+++ Gestiune magazin c#/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace GestiuneMagazin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Magazin magazin = new Magazin();
+            int choice;
+
+            do
+            {
+                Console.WriteLine("1. Adauga aliment");
+                Console.WriteLine("2. Modifica aliment");
+                Console.WriteLine("3. Sterge aliment");
+                Console.WriteLine("4. Afiseaza aliment");
+                Console.WriteLine("5. Cauta aliment");
+                Console.WriteLine("6. Iesire");
+
+                Console.Write("Alege optiunea: ");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        magazin.AdaugaAliment();
+                        break;
+                    case 2:
+                        magazin.ModificaAliment();
+                        break;
+                    case 3:
+                        magazin.StergeAliment();
+                        break;
+                    case 4:
+                        magazin.AfiseazaAliment();
+                        break;
+                    case 5:
+                        magazin.CautaAliment();
+                        break;
+                    case 6:
+                        Console.WriteLine("Iesire din program...");
+                        break;
+                    default:
+                        Console.WriteLine("Optiune invalida, incearca din nou.");
+                        break;
+                }
+
+                Console.WriteLine();
+            } while (choice != 6);
+        }
+    }
+}
